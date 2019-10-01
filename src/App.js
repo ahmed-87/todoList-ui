77e4be35fac2ee7b01243225be53cf39747d4886
Ahmed Hameed <ahmed.hameed@colorado.edu>
2019-09-30 23:52:19 -0700

    updated src/App.js with toast message component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,41 @@ import Body from './Body';
 import { BrowserRouter as Router } from "react-router-dom";
 import LoadingMask from './LoadingMask';
 import {connect} from 'react-redux';
+import { SemanticToastContainer, toast } from 'react-semantic-toasts';
 
 class App extends React.Component {
 
-  componentDidMount(){
+  componentDidUpdate(prevProps) {
+    const {toastDetails} = this.props;
+    if(prevProps.toastDetails !== toastDetails){
+      toast({ 
+          size : 'medium',
+          time: 5000,
+          type: toastDetails.type,
+          title: toastDetails.title,
+          description : toastDetails.description
+        });
+      }
 
   }
 
   render() {
+    const {isLoading, message, isSignedIn, profile} = this.props;
+    let user = { isSignedIn, profile }
     return (
       <div>
-      <LoadingMask {...this.props}/>
+        <LoadingMask isLoading={isLoading} message={message}/>
         <Router>
           <div id="header">
-            <Header />
+            <Header user={user} />
           </div>
-          <div id="body">
-            <Body />
+          <div id="body" >
+            <Body user={user}/>
+          </div>
+          <div id="footer">
+            <div className="toast" style={{position: 'absolute', bottom: 0, right: 0, width: 300}}>
+              <SemanticToastContainer />
+            </div>
           </div>
         </Router>
       </div>
@@ -29,8 +47,10 @@ class App extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  const {isLoading, message} = state.util;
-    return {isLoading, message};
+  const {isLoading, message, toastDetails} = state.util;
+  const {isSignedIn, profile} = state.auth;
+
+  return {isLoading, message, toastDetails, isSignedIn, profile};
 }
 
 export default connect(mapStateToProps)(App);
